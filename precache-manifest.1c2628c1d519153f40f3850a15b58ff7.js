26d820eacbe8a853535e5d2ab4eddc4b4f97032e
Maggie Chan <mach.sq@gmail.com>
2018-10-26 17:25:14 -0400

    Updates

---
+++ precache-manifest.1c2628c1d519153f40f3850a15b58ff7.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "93ed674629f5eb1fbc5ef15b19bd5c30",
+    "url": "/ChiangMai/static/media/2.93ed6746.jpg"
+  },
+  {
+    "revision": "b68c213cee212a1ae7d1",
+    "url": "/ChiangMai/static/css/main.9d69f8b0.chunk.css"
+  },
+  {
+    "revision": "e1ada48e35de0e3b8be4",
+    "url": "/ChiangMai/static/js/1.e1ada48e.chunk.js"
+  },
+  {
+    "revision": "5a29a6f5660c7d459b2b",
+    "url": "/ChiangMai/static/js/runtime~main.5a29a6f5.js"
+  },
+  {
+    "revision": "76c5312cbaf589af6d5c1c892a7fef38",
+    "url": "/ChiangMai/static/media/hero.76c5312c.jpg"
+  },
+  {
+    "revision": "20e45ac6db876a7e2618aba0470b9c35",
+    "url": "/ChiangMai/static/media/hero.20e45ac6.jpg"
+  },
+  {
+    "revision": "240e16df4470ba322190563444c84c0e",
+    "url": "/ChiangMai/static/media/hero.240e16df.jpg"
+  },
+  {
+    "revision": "a24b1370a525003e920048f4a239ead7",
+    "url": "/ChiangMai/static/media/hero.a24b1370.jpg"
+  },
+  {
+    "revision": "3af5f7aa682a203a4a1e8cc0cc9efdc9",
+    "url": "/ChiangMai/static/media/hero.3af5f7aa.jpg"
+  },
+  {
+    "revision": "5bc31a8ba1c5ffd812532b4cd906cb37",
+    "url": "/ChiangMai/static/media/1.5bc31a8b.jpg"
+  },
+  {
+    "revision": "7798447ecc3fd489270d8e2125e22918",
+    "url": "/ChiangMai/static/media/1.7798447e.jpg"
+  },
+  {
+    "revision": "d0287f8bf095081d30b170135dd4e716",
+    "url": "/ChiangMai/static/media/1.d0287f8b.jpg"
+  },
+  {
+    "revision": "01b3d102ee21e5ecb5c225374657c14c",
+    "url": "/ChiangMai/static/media/1.01b3d102.jpg"
+  },
+  {
+    "revision": "4adf6d8c5887377dc28ae3eab997f840",
+    "url": "/ChiangMai/static/media/1.4adf6d8c.jpg"
+  },
+  {
+    "revision": "9e219ef2bb3e97ce135cbed8301e05c9",
+    "url": "/ChiangMai/static/media/2.9e219ef2.jpg"
+  },
+  {
+    "revision": "b68c213cee212a1ae7d1",
+    "url": "/ChiangMai/static/js/main.b68c213c.chunk.js"
+  },
+  {
+    "revision": "8cf03a331829bda99a4c0460d5a34f2d",
+    "url": "/ChiangMai/static/media/2.8cf03a33.jpg"
+  },
+  {
+    "revision": "fd8cc87732a87f10ee104a7b48708ee6",
+    "url": "/ChiangMai/static/media/2.fd8cc877.jpg"
+  },
+  {
+    "revision": "28cff252ecf8ca7bc2e071df2bad312e",
+    "url": "/ChiangMai/static/media/2.28cff252.jpg"
+  },
+  {
+    "revision": "25836a936909241635fb69c28838d537",
+    "url": "/ChiangMai/static/media/footer.25836a93.jpg"
+  },
+  {
+    "revision": "faf9ac74704342da763674ccb7251f1b",
+    "url": "/ChiangMai/static/media/footer.faf9ac74.jpg"
+  },
+  {
+    "revision": "7c2ea7cd7dfcac6681df63e4921a997c",
+    "url": "/ChiangMai/static/media/footer.7c2ea7cd.jpg"
+  },
+  {
+    "revision": "850df8e5e6993eeadfa49d0636db16a6",
+    "url": "/ChiangMai/static/media/footer.850df8e5.jpg"
+  },
+  {
+    "revision": "b299b0fec54d559f27e066ecad73331f",
+    "url": "/ChiangMai/static/media/footer.b299b0fe.jpg"
+  },
+  {
+    "revision": "76c5312cbaf589af6d5c1c892a7fef38",
+    "url": "/ChiangMai/static/media/header.76c5312c.jpg"
+  },
+  {
+    "revision": "662781f80ee2c3bd27966a94faadd0db",
+    "url": "/ChiangMai/static/media/header.662781f8.jpg"
+  },
+  {
+    "revision": "8cf03a331829bda99a4c0460d5a34f2d",
+    "url": "/ChiangMai/static/media/header.8cf03a33.jpg"
+  },
+  {
+    "revision": "4189874fc499d6221bf3c07f8b0f20c9",
+    "url": "/ChiangMai/static/media/header.4189874f.jpg"
+  },
+  {
+    "revision": "fe5e791297e6e0aa2f7ef88e430fb69e",
+    "url": "/ChiangMai/static/media/header.fe5e7912.jpg"
+  },
+  {
+    "revision": "4fb30d195fed0798888fa29f090396ad",
+    "url": "/ChiangMai/index.html"
+  }
+];
